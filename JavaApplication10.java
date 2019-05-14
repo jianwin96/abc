9423f93222b8f6c3a118481c1a644bcfe8950b4d
jianwin96 <32693736+jianwin96@users.noreply.github.com>
2019-05-15 01:29:08 +0800

    First Test

---
+++ JavaApplication10.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+import static java.time.Clock.system;
+
+/**
+ *
+ * @author WeeKien96
+ */
+public class JavaApplication10 {
+static int a = 5;
+static int b = 10;
+static int c = 4;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int result;
+      
+       
+       result = a + b + c;
+       
+       System.out.println(result);
+       
+        
+    }
+
+    private static void print(int result) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
